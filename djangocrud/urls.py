23c073d69445fad1a250a556677787895927e0ff
Michael <76667453+micdev11@users.noreply.github.com>
2025-05-16 17:57:28 -0400

    corrections createtask

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('tasks/<int:task_id>', views.task_detail, name='task_detail'),
     path('taks/<int:task_id>/complete', views.complete_task, name='complete_task'),
     path('tasks/<int:task_id>/delete', views.delete_task, name='delete_task'),
+    path('tasks/create/', views.create_task, name='create_task'),
 ]
--- tasks/forms.py
+++ tasks/forms.py
@@ -1,13 +1,13 @@
-from django.forms import ModelForm
+# from django.forms import ModelForm
 from .models import Task
 from django import forms
 
-class TaskForm(ModelForm):
+class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description', 'important']
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter task title'}),
-            'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter task description'}),
-            'important': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Título de la tarea'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Descripción de la tarea'}),
+            'important': forms.CheckboxInput(attrs={'class': 'form-check-input m-auto'})
         }
